dc34fed9c8245c4c2bed75ade6defdb53d66fc46
Tim Zimmermann <tim@linux4.de>
2021-10-14 17:35:27 +0200

    beyondx: switch to UDFPS
    Change-Id: I8847f5d47e1c641f3c1e70f45289861d5773b857

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,6 @@ DEVICE_PATH := device/samsung/beyondx
 # APEX image
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
-TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
-
 ## Display
 TARGET_SCREEN_DENSITY := 420
 
